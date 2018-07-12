1438bc52fa227141930a32c5ee851a7a0a5eaca9
Chris Macnichol <chrismacnichol@gmail.com>
2018-07-11 21:52:05 -0400

    no message

--- examples/mitsubishi_heatpump_mqtt_esp8266/mitsubishi_heatpump_mqtt_esp8266.h
+++ examples/mitsubishi_heatpump_mqtt_esp8266/mitsubishi_heatpump_mqtt_esp8266.h
@@ -2,25 +2,25 @@
 //#define OTA
 
 // wifi settings
-const char* ssid     = "<YOUR WIFI SSID GOES HERE>";
-const char* password = "<YOUR WIFI PASSWORD GOES HERE>";
+const char* ssid     = "Avalon";
+const char* password = "ZetaCygni";
 
 // mqtt server settings
-const char* mqtt_server   = "<YOUR MQTT BROKER IP/HOSTNAME GOES HERE>";
+const char* mqtt_server   = "192,168,2,5";
 const int mqtt_port       = 1883;
-const char* mqtt_username = "<YOUR MQTT USERNAME GOES HERE>";
-const char* mqtt_password = "<YOUR MQTT PASSWORD GOES HERE>";
+const char* mqtt_username = "node";
+const char* mqtt_password = "camibm";
 
 // mqtt client settings
 // Note PubSubClient.h has a MQTT_MAX_PACKET_SIZE of 128 defined, so either raise it to 256 or use short topics
-const char* client_id                   = "heatpump-controller-1"; // Must be unique on the MQTT network
-const char* heatpump_topic              = "heatpump";
-const char* heatpump_set_topic          = "heatpump/set";
-const char* heatpump_status_topic       = "heatpump/status";
-const char* heatpump_timers_topic       = "heatpump/timers";
+const char* client_id                   = "HVAC-1"; // Must be unique on the MQTT network
+const char* heatpump_topic              = "HVAC/UpStairs";
+const char* heatpump_set_topic          = "HVAC/UpStairs/set";
+const char* heatpump_status_topic       = "HVAC/UpStairs/status";
+const char* heatpump_timers_topic       = "HVAC/UpStairs/timers";
 
-const char* heatpump_debug_topic        = "heatpump/debug";
-const char* heatpump_debug_set_topic    = "heatpump/debug/set";
+const char* heatpump_debug_topic        = "HVAC/UpStairs/debug";
+const char* heatpump_debug_set_topic    = "HVAC/UpStairs/debug/set";
 
 // pinouts
 const int redLedPin  = 0; // Onboard LED = digital pin 0 (red LED on adafruit ESP8266 huzzah)
